b767956b64626fdd3a373d0d40e0e262277b9777
pjanevski <pjanevski@tenstorrent.com>
2026-03-17 10:56:28 +0000

    Move the flag out of PCI and TT device

--- device/api/umd/device/tt_device/wormhole_tt_device.hpp
+++ device/api/umd/device/tt_device/wormhole_tt_device.hpp
@@ -65,8 +65,7 @@ protected:
     void dma_h2d_transfer(const uint32_t dst, const uint64_t src, const size_t size) override;
 
 private:
-    friend std::unique_ptr<TTDevice> TTDevice::create(
-        int device_number, IODeviceType device_type, bool use_safe_api, bool low_power);
+    friend std::unique_ptr<TTDevice> TTDevice::create(int device_number, IODeviceType device_type, bool use_safe_api);
 
     bool is_hardware_hung() override;
 
